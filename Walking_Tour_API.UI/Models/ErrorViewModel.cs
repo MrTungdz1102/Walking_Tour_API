dcdb97a2bc6e269a7dbe888466cfbd60e31b21ea
Tùng Đào <tung201210402@lms.utc.edu.vn>
2023-06-20 06:15:59 +0700

    Add project files.

---
+++ Walking_Tour_API.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Walking_Tour_API.UI.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
